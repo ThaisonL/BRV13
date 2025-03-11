cdc14cd554ba2df79f9dab5c2be8adfd3a03a90c
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-03-11 14:11:38 +0100

    Dashboard branch initialized

--- br-v9-v13/src/pages/Home.jsx
+++ br-v9-v13/src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <main className="">
       <h1 className="bg-red-600">Denna sida suckaaaasssss</h1>
-      <p className="bg-red-600">Utforska nya ekonomiska trender!</p>
+      <h2 className="bg-red-600">Utforska nya ekonomiska trender!</h2>
     </main>
   );
 }
