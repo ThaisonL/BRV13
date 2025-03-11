857fc99eb791893afa7cd53086e0dabe90636438
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-03-11 14:02:09 +0100

    Develop branch added

--- br-v9-v13/src/pages/Home.jsx
+++ br-v9-v13/src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <main className="">
       <h1 className="bg-red-600">Denna sida suckaaaasssss</h1>
-      <p className="bg-red-600">Utforska ny musik!</p>
+      <p className="bg-red-600">Utforska nya ekonomiska trender!</p>
     </main>
   );
 }
