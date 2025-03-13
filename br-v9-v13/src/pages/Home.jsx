f0bc31b5b02bd310a43089db97e6457823da8c9b
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-03-13 14:25:06 +0100

    merge now succes

--- br-v9-v13/src/pages/Home.jsx
+++ br-v9-v13/src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import AppleStock from "../components/ApplestockChange/AppleStock";
+import BusinessNewsSection from "../components/BusinessNews/BusinessNewsSection";
 function Home() {
   return (
-    <main className="">
-      <h1 className="bg-red-600">Denna sida suckaaaasssss</h1>
-      <p className="bg-red-600">Utforska nya ekonomiska trender!</p>
+    <main className="flex flex-col justify-center items-center bg-[#faebd7]">
+      <h1 className="opacity-0">Denna sida suckaaaasssss</h1>
+
+      <AppleStock />
+      <BusinessNewsSection />
     </main>
   );
 }
