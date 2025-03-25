ddf0fe3b2b7bd3a247e8c1b007f48cc8280a422c
Phithai <phi-thai.lai@chasacademy.se>
2025-03-25 07:39:54 +0100

    fixed with css on NewsItem and made the cards symetrical

--- br-v9-v13/src/components/BusinessNews/NewsItem.jsx
+++ br-v9-v13/src/components/BusinessNews/NewsItem.jsx
@@ -4,25 +4,30 @@ function NewsItem({ new: newsItem }) {
   return (
     <article className="hover:scale-105 bg-white w-full my-10 m-4 p-3 box-content shadow-lg rounded-md md:w-5/12 xl:w-3/12 min-h-[500px] flex flex-col">
       <h4 className="font-semibold truncate">{newsItem.title}</h4>
-      <img
-        className="w-full h-[200px] object-cotain"
-        src={newsItem.urlToImage || fallbackImage}
-        alt={newsItem.title || "Default News Image"}
-        onError={(e) => e.target.src = fallbackImage}
-      />
+      
+      {/* Bilden inuti en figure */}
+      <figure className="w-full h-[200px] flex justify-center items-center">
+        <img
+          className="max-w-full max-h-full object-contain"
+          src={newsItem.urlToImage || fallbackImage}
+          alt={newsItem.title || "Default News Image"}
+          onError={(e) => e.target.src = fallbackImage}
+        />
+      </figure>
+
       <p>{newsItem.publishedAt.replace("T", " ").replace("Z", " ")}</p>
       <p className="flex-grow overflow-hidden text-ellipsis">{newsItem.description}</p>
-      <div className="mt-auto"> {/* Detta håller "Full article" alltid längst ner */}
-        <p>Full article: </p>
-        <a
-          className="italic underline text-sm break-all overflow-hidden text-ellipsis"
-          href={newsItem.url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {newsItem.url}
-        </a>
-      </div>
+      
+      {/* Full article alltid längst ner */}
+      <p>Full article: </p>
+      <a
+        className="italic underline text-sm break-all overflow-hidden text-ellipsis"
+        href={newsItem.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {newsItem.url}
+      </a>
     </article>
   );
 }
