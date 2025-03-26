f6576d00a1eaeb90632b29f63d4e28a719790473
Phithai <phi-thai.lai@chasacademy.se>
2025-03-26 15:15:38 +0100

    merge successful with PL/Individuell branch

--- br-v9-v13/tailwind.config.js
+++ br-v9-v13/tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
     content: [
-        "./src/pages/**/*.{js,jsx}", // Kollar alla sidor
-        "./src/components/**/*.{js,jsx}", // Kollar alla komponenter
-        "./src/app/**/*.{js,jsx}", // Kollar appen om den finns i en separat mapp
-    ],
-    theme: {
-        extend: {
-            colors: {
+        "./src/pages/**/*.{js,jsx}", // Include all JS/JSX files in the pages directory
+        "./src/components/**/*.{js,jsx}", // Include all components
+        "./src/app/**/*.{js,jsx}", // Include app-related files
+        theme: {
+            extend: {
+                colors: {
+                },
             },
         },
-    },
-    plugins: [],
-    darkMode: 'class', // enables Dark Mode with ' dark ' class
+        plugins: [],
+        darkMode: 'class', // enables Dark Mode with ' dark ' class
 };
